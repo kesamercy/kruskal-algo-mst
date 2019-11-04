bf776fecde1cadfff9de52bacebb0ba54eea4269
Mercy Nekesa <kesamercy@gmail.com>
2019-11-03 19:35:55 -0500

    fairly working implementation,buggy, but i have hope it might work eventually.

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -10,21 +10,25 @@ import java.util.PriorityQueue;
 public class Solution {
 
     private ArrayList<ArrayList<Integer>> adj_matrix;
+    private ArrayList<ArrayList<Integer>> sets = new ArrayList<>();
+    int[] output;
 
     public Solution(ArrayList<ArrayList<Integer>> adj_matrix) {
         this.adj_matrix = adj_matrix;
     }
 
-    class Union implements Comparable<Union> {
+
+    class Nodes implements Comparable<Nodes> {
         private int edgeWeight;
         private int parentNode;
         private int childNode;
 
-        public Union(int edgeWeight, int parentNode, int childNode) {
+        public Nodes(int edgeWeight, int parentNode, int childNode) {
             this.edgeWeight = edgeWeight;
             this.parentNode = parentNode;
             this.childNode = childNode;
         }
+
         @Override
         public String toString() {
             return "Union{" +
@@ -34,8 +38,8 @@ public class Solution {
         }
 
         @Override
-        public int compareTo(Union edge) {
-            return (this.edgeWeight - edge.edgeWeight);
+        public int compareTo(Nodes node) {
+            return (this.edgeWeight - node.edgeWeight);
         }
     }
 
@@ -44,11 +48,9 @@ public class Solution {
          * Output the node ids of the smallest weighted path.
          */
         int sizeOfAdjMatrix = adj_matrix.size();
-        boolean[] visitedNodes = new boolean[sizeOfAdjMatrix];
-        int[] output = new int[sizeOfAdjMatrix];
-        PriorityQueue<Union> pq = new PriorityQueue<>();
-        int rootnode = 0;
-        int initialUnion = 0;
+        PriorityQueue<Nodes> pq = new PriorityQueue<>();
+        output = new int[sizeOfAdjMatrix];
+
 
         for (int i = 0; i < output.length; i++) {
             output[i] = -1;
@@ -58,67 +60,62 @@ public class Solution {
             for (int child = 0; child < sizeOfAdjMatrix; child++) {
                 int edgeWeight = adj_matrix.get(parent).get(child);
                 if (edgeWeight != -1) {
-                    pq.add(new Union(edgeWeight, parent, child));
+                    pq.add(new Nodes(edgeWeight, parent, child));
                 }
             }
         }//end for the adj list
-
-
-        while (!pq.isEmpty()){
-            Union nodeComponent = pq.poll();
-            int nodeOne = nodeComponent.parentNode;
-            int nodeTwo = nodeComponent.childNode;
-            //since we know that 0 will always be the root node... then we can say..
-
-            if (output[nodeOne] == -1 && output[nodeTwo] == -1) {
-                ++initialUnion;
-                //this means that they are parents of themselves.... and they belong to 2 diff. sets.
-                //define their parent and child relationship
-                output[nodeTwo] = nodeOne;
-                output[nodeOne] = output[rootnode];
-                //root node changes when there is a union. if union, increment the counter
-                if (initialUnion == 1) {
-                    output[rootnode] = 0;
-                    output[rootnode] = output[rootnode] - 2;
-                }
-            }
-            else {
-                //find the parents of node one and node 2, do this so we can find if there is a union
-                int node1parent = findParent(nodeOne, output);
-                int node2parent = findParent(nodeTwo, output);
-
-                if ( node1parent != node2parent){
-                    //a union can happen in this instance
-                    //find the value at the index of the parent node
-                    int rootvalue = output[node1parent];
-                    int otherRootValue = output[node2parent];
-
-                    if (rootvalue <= otherRootValue) {
-                        output[node2parent] = node1parent;
-
-                    }
-                    else {
-                        output[node1parent] = node2parent;
-                    }
-
-                    output[rootnode] = output[rootnode] - 2;
-
-                }
-
-            }
-
+        for (int i = 0; i < sizeOfAdjMatrix; i++) {
+            ArrayList<Integer> setOfNodes = new ArrayList<>();
+            setOfNodes.add(i);
+            sets.add(setOfNodes);
         }
 
-        output[rootnode] = -1;
+        while (!pq.isEmpty()) {
+            Nodes removedNode = pq.poll();
+            int childNode = removedNode.childNode;
+            int parentNode = removedNode.parentNode;
+
+            union(childNode, parentNode);
+        }
+
         return output;
     }
 
-    public int findParent(int node, int[] output){
+    public void union(int childNode, int parentNode) {
+        int setIndexOfChildNode = findSet(childNode);
+        int setIndexOfParentNode = findSet(parentNode);
 
-        if (output[node] <= 0) {
-            return node;
+        if (setIndexOfParentNode != setIndexOfChildNode) {
+            //CHECK TO SEE IF IT WORKS AS IT SHOULD
+            for (int element : sets.get(setIndexOfChildNode)) {
+                sets.get(setIndexOfParentNode).add(element);
+            }
+            //CHECK TO SEE IF IT WORKS AS IT SHOULD!!!!!!
+            sets.remove(setIndexOfChildNode);
+            if (output[childNode] == -1) {
+                output[childNode] = parentNode;
+            } else {
+                System.out.println("You have a serious bug, the child node seems to have 2 parents" + childNode + " "+ parentNode);
+            }
         }
 
-        return findParent(output[node], output);
+
     }
+
+    public int findSet(int node) {
+        int setIndex = 0;
+
+        //TEST TO SEE IF IT WORKS AS IT SHOULD
+        for (int i = 0; i < sets.size() ; i++) {
+            setIndex = sets.get(i).indexOf(node);
+
+            if (setIndex != -1) {
+                return i;
+            }
+
+        }
+
+        return setIndex;
+    }
+
 }
